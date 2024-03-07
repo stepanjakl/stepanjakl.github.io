661afadc54f6ce7c189b90dd3c11a0d2a3a7daff
Stepan Jakl <stepan.jakl@outlook.com>
2024-03-07 15:04:27 +0000

    update timeline web component

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -13,7 +13,7 @@ class timeline extends HTMLElement {
         this[property] = stringToArray(newValue)
     }
 
-    /* TODO add variables, use segment where possible */
+    /* TODO add variables, use --segment where possible */
     connectedCallback() {
         this.innerHTML = `
             <style>
@@ -23,7 +23,7 @@ class timeline extends HTMLElement {
                 }
 
                 #timeline_wrapper {
-                    transition: border-radius var(--animate-out-segment) var(--ease-in-quad), margin var(--animate-out-segment) var(--ease-in-quad);
+                    transition: border-radius var(--animate-out-segment) var(--ease-in-quad), margin var(--animate-out-segment) var(--ease-in-quad), transform var(--animate-out-segment) var(--ease-in-quad);
                     position: relative;
                     margin: 0 128px;
                     border-radius: 12px;
@@ -34,28 +34,32 @@ class timeline extends HTMLElement {
                 }
 
                 #timeline_wrapper:hover {
-                    transition: border-radius var(--animate-in-segment) var(--ease-out-quad), margin var(--animate-in-segment) var(--ease-out-quad);
+                    transition: border-radius var(--animate-in-segment) var(--ease-out-quad), margin var(--animate-in-segment) var(--ease-out-quad), transform var(--animate-in-segment) var(--ease-out-quad);
                     border-radius: 18px;
                     margin: 0 64px;
+                    transform: translateY(calc(18px / 4))
                 }
 
                 #timeline_content {
                     transition: padding var(--animate-out-segment) var(--ease-in-quad);
-                    // display: flex;
-                    // align-items: center;
-                    // justify-content: center;
-                    // white-space: nowrap;
-                    overflow-x: auto;
+                    overflow-x: scroll;
                     overflow-y: hidden;
                     scroll-behavior: smooth;
+                    white-space: nowrap;
+                    scrollbar-width: none;
                     -webkit-overflow-scrolling: touch;
+                    -ms-overflow-style: none;
                     padding: 8px 24px 4px 24px;
                     mask-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 1) var(--segment), rgba(0, 0, 0, 1) calc(100% - var(--segment)), rgba(0, 0, 0, 0) 100%);
                 }
 
+                #timeline_content::-webkit-scrollbar {
+                    display: none;
+                }
+
                 #timeline_wrapper:hover #timeline_content {
                     transition: padding var(--animate-in-segment) var(--ease-out-quad);
-                    padding: 16px calc(24px + 64px) 18px calc(24px + 64px);
+                    padding: calc(8px + 6px) calc(24px + 64px) calc(4px + 6px) calc(24px + 64px);
                 }
 
                 #timeline_content > div {
@@ -65,7 +69,7 @@ class timeline extends HTMLElement {
 
                 #timeline_wrapper:hover #timeline_content > div {
                     transition: row-gap var(--animate-in-segment) var(--ease-out-quad);
-                    row-gap: 12px;
+                    row-gap: calc(8px + 6px);
                 }
 
                 #timeline {
@@ -96,10 +100,6 @@ class timeline extends HTMLElement {
                 #timeline span:first-child, #timeline span:last-child {
                     background-color: rgba(255, 255, 255, 0.25);
                 }
-
-                // #timeline span:first-child {
-                //     opacity: 0;
-                // }
 
                 #timeline span.active,
                 #timeline span.active + span,
@@ -150,11 +150,9 @@ class timeline extends HTMLElement {
 
                 #timeline_labels button {
                     position: relative;
-                    // scroll-snap-align: start;
                     display: inline-flex;
                     justify-self: center;
                     cursor: pointer;
-                    // transform: translate(-50%);
                 }
 
                 #timeline_labels button span {
@@ -202,7 +200,7 @@ class timeline extends HTMLElement {
                     inset: 0;
                 }
 
-                #timeline_labels button:hover::before {
+                #timeline_labels button:hover::before, #timeline_labels button:focus::before {
                     transition: opacity var(--animate-in-segment-2\\/3) linear, inset var(--animate-in-segment-2\\/3) var(--ease-out-quad);
                     opacity: 1;
                     inset: 0;
@@ -237,77 +235,70 @@ class timeline extends HTMLElement {
             </div>
         `
 
-        /* TODO review the function below & make sure when button gets clicked and mouse quickly removed, there's not lag -- add .active straight away */
 
+        const scrollParentToChildCenterHorizontal = (parent, child) => {
+            var parentRect = parent.getBoundingClientRect()
+            var childRect = child.getBoundingClientRect()
+            var scrollAmount = childRect.left - parentRect.left - (parentRect.width - childRect.width) / 2
+            parent.scrollLeft += scrollAmount
+        }
 
-        const elements = Array.from(document.querySelectorAll('[data-label-for]'))
+        const scrollParentToChildVertical = (parent, child) => {
+            const parentRect = parent.getBoundingClientRect()
+            const childRect = child.getBoundingClientRect()
+            const scrollAmount = childRect.top - parentRect.top - 24
+            parent.scrollTop += scrollAmount
+        }
 
-        elements.forEach(element => {
-            element.addEventListener('click', () => {
-                const sectionLabel = element.getAttribute('data-label-for')
-                const targetElement = document.querySelector(`[data-timeline-section="${sectionLabel}"]`)
-
-                if (targetElement) {
-                    scrollParentToChildCenterVertical(document.querySelector('#modal_archive'), targetElement)
-                }
-            })
-        })
-
-        const intersectionObserver = new IntersectionObserver(entries => entries.forEach(entry => {
-            const sectionLabel = entry.target.getAttribute('data-timeline-section')
-            const labelEl = this.querySelector(`[data-label-for="${sectionLabel}"]`)
-            const labelEls = Array.from(this.querySelectorAll('[data-label-for]'))
-            const timelineEls = Array.from(this.querySelectorAll('#timeline span'))
-            if (entry.isIntersecting) {
-                scrollParentToChildCenterHorizontal(this.querySelector('#timeline_content'), labelEl)
-                labelEls.forEach(element => { element.classList.remove('active') })
-                labelEl.classList.add('active')
-                labelEls.some((element, index) => {
-                    if (element.getAttribute('data-label-for') === sectionLabel) {
-                        timelineEls.forEach(element => { element.classList.remove('active') })
-                        if (index === 0) {
-                            timelineEls[3].classList.add('active')
-                        }
-                        else {
-                            timelineEls[3 + (index * 6)].classList.add('active')
-                        }
-                    }
-                })
+        const handleLabelClick = (modalArchiveEl, labelEl) => () => {
+            const section = labelEl.getAttribute('data-label-for')
+            const targetElement = document.querySelector(`[data-timeline-section="${section}"]`)
+            if (targetElement) {
+                scrollParentToChildVertical(modalArchiveEl, targetElement)
             }
-        }), { threshold: 0.75 })
-        document.querySelectorAll('[data-timeline-section]').forEach(element => intersectionObserver.observe(element))
+        }
 
-        /* let entryElWidthPrev = null
-        const resizeObserver = new ResizeObserver((entries) => {
+        const handleIntersection = (timelineContentEl, labelEls) => (entries) => {
             entries.forEach((entry) => {
-                const entryElWidth = entry.contentRect.width
-                const timelineContent = this.querySelector('#timeline_content')
-                const contentHorizontalScroll = timelineContent.scrollLeft
+                const targetSection = entry.target.getAttribute('data-timeline-section')
+                const targetLabelEl = document.querySelector(`[data-label-for="${targetSection}"]`)
+                const timelineEls = Array.from(document.querySelectorAll('#timeline span'))
 
-                console.log(entryElWidth, entryElWidthPrev, contentHorizontalScroll)
+                if (entry.isIntersecting) {
+                    targetLabelEl.focus()
+                    scrollParentToChildCenterHorizontal(timelineContentEl, targetLabelEl)
 
-                // const isScrolledToLeft = contentHorizontalScroll === 0
-                // const isScrolledToRight = contentHorizontalScroll === (timelineContent.scrollWidth - timelineContent.clientWidth)
+                    labelEls.forEach((labelEl) => labelEl.classList.remove('active'))
+                    targetLabelEl.classList.add('active')
 
-                if (entryElWidthPrev !== null && entryElWidth !== entryElWidthPrev) {
-                    const scrollAdjustment = (entryElWidth - entryElWidthPrev) / 2
-                    timelineContent.scrollTo({
-                        left: contentHorizontalScroll - scrollAdjustment,
-                        behavior: 'instant'
-                    })
+                    const index = labelEls.findIndex((labelEl) => labelEl.getAttribute('data-label-for') === targetSection)
+                    timelineEls.forEach((timelineEl) => timelineEl.classList.remove('active'))
+                    timelineEls[3 + (index === 0 ? 0 : index * 6)].classList.add('active')
                 }
-                entryElWidthPrev = entryElWidth
             })
-        })
-        resizeObserver.observe(this.querySelector('#timeline_wrapper')) */
+        }
+
+        const setupIntersectionObserver = (timelineContentEl, labelEls) => {
+            const intersectionObserver = new IntersectionObserver(handleIntersection(timelineContentEl, labelEls), { threshold: 0.75 })
+            document.querySelectorAll('[data-timeline-section]').forEach((element) => intersectionObserver.observe(element))
+        }
+
+        const initializeTimeline = () => {
+            const modalArchiveEl = document.querySelector('#modal_archive')
+            const timelineContentEl = document.querySelector('#timeline_content')
+            const labelEls = Array.from(document.querySelectorAll('[data-label-for]'))
+
+            labelEls.forEach((labelEl) => {
+                labelEl.addEventListener('click', handleLabelClick(modalArchiveEl, labelEl))
+            })
+
+            setupIntersectionObserver(timelineContentEl, labelEls)
+        }
+
+        initializeTimeline()
+
     }
 }
-
-/* function round(value, step) {
-    step || (step = 1.0);
-    var inverse = 1.0 / step;
-    return Math.round(value * inverse) / inverse;
-} */
 
 const stringToArray = (inputString) => {
     const match = inputString.match(/\[(.*?)\]/)
@@ -315,20 +306,6 @@ const stringToArray = (inputString) => {
         return match[1].split(',').map(item => item.trim())
     }
     return []
-}
-
-const scrollParentToChildCenterHorizontal = (parent, child) => {
-    var parentRect = parent.getBoundingClientRect()
-    var childRect = child.getBoundingClientRect()
-    var scrollAmount = childRect.left - parentRect.left - (parentRect.width - childRect.width) / 2
-    parent.scrollLeft += scrollAmount
-}
-
-const scrollParentToChildCenterVertical = (parent, child) => {
-    const parentRect = parent.getBoundingClientRect()
-    const childRect = child.getBoundingClientRect()
-    const scrollAmount = childRect.top - parentRect.top - (parentRect.height - childRect.height) / 2
-    parent.scrollTop += scrollAmount
 }
 
 customElements.define('horizontal-timeline', timeline)
